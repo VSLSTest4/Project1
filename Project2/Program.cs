68816f04812110c43d058832bf19a38213e69ace
Joanna Zheng <v-joazh@microsoft.com>
2021-07-18 05:43:00 -0700

    Add project files.

---
+++ Project2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Project2
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
